97b6c277b2d7b16eb67f870d9a97f24328c0a413
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 09:19:42 -0600

    Add tests for is_hoarded and hoard methods

--- test/product_test.rb
+++ test/product_test.rb
@@ -40,4 +40,28 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_it_is_hoarded
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+    assert_equal false, product1.is_hoarded?
+    assert_equal false, product2.is_hoarded?
+
+  end
+
+  def test_it_can_be_hoarded
+  product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+  product2 = Product.new(:meat, 'chicken', 4.50, '2')
+
+  product1.hoard
+
+  assert_equal true, product1.is_hoarded?
+
+  product2.hoard
+
+  assert_equal true, product2.is_hoarded?
+
+  end
+
 end
