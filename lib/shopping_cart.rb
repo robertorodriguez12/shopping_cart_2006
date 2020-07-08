3b469e314f0d051a09b5b42a8004a78b0c05f90d
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 10:48:14 -0600

    Complete iteration 3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,11 +1,18 @@
+require './lib/product'
+
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :is_full
 
   def initialize(name, capacity)
     @name = name
-    @capacity = capacity
+    @capacity = capacity.to_i
     @products = []
+    @is_full = false
 
+  end
+
+  def quantity
+     @products.sum{|product| product.quantity}
   end
 
   def add_product(product)
@@ -14,6 +21,11 @@ class ShoppingCart
 
   def details
     {name:@name, capacity:@capacity}
+  end
+
+  def is_full?
+    return true if quantity > 30
+    return false if quantity < 30
   end
 
 end
