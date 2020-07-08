eb38ad81ae731a1850e2bd7c71ffdc323f5df2bc
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 09:02:38 -0600

    Get first three tests to pass

--- test/product_test.rb
+++ test/product_test.rb
@@ -10,13 +10,13 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
 
   def test_it_has_a_name
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 'toilet paper', product.name
   end
