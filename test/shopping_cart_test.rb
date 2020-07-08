3d2a4bcc22cfb98fde9bb231d44f0d34c6a77dda
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 10:50:35 -0600

    Complete most of iteration 3

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -39,4 +39,33 @@ class ShoppingCartTest < MiniTest::Test
     cart.details
   end
 
+  def test_shoppig_cart_is_not_full
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal false, cart.is_full?
+  end
+
+  def test_shoppint_cart_can_be_full
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+    # binding.pry
+    assert_equal true, cart.is_full?
+
+
+  end
 end
