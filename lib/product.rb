0b21954aeb5a3ca967c0992bb21eb5614c09ef3c
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 10:47:58 -0600

    Update attribute

--- lib/product.rb
+++ lib/product.rb
@@ -5,10 +5,15 @@ class Product
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity.to_i
+    @quantity = quantity
     @is_hoarded = false
 
   end
+
+  def quantity
+    @quantity.to_i
+  end
+
 
   def total_price
     return unit_price * quantity
