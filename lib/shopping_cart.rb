b888accbd5054dcff040c6746eaea3421a056db1
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-08 09:35:44 -0600

    Define add_product

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,15 @@
 class ShoppingCart
-  attr_reader :name, :capacity
+  attr_reader :name, :capacity, :products
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @products = []
 
+  end
+
+  def add_product(product)
+    @products << product
   end
 
 end
